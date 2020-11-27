d84ac1036b928d07f0c724ba870bccfb9f89e84b
Jonathan Ibrahim <jonathan.ibrahim@binus.edu>
2020-11-27 11:35:11 +0700

    Add Json Extension to add new method for JsonElement

---
+++ DevDrillAPI/Helper/Extensions/JsonExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DevDrillAPI.Helper.Extensions
+{
+    public static class JsonExtension
+    {
+        public static T GetObject<T>(this JsonElement element)
+        {
+            var json = element.GetRawText();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+        public static T GetObject<T>(this JsonDocument document)
+        {
+            var json = document.RootElement.GetRawText();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
